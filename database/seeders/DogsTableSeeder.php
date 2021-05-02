3549308e293af61848d8ee96687caf6151ea8d2b
Johan Tovar <jtovarto@gmail.com>
2021-05-02 15:45:08 -0400

    leccion-01

---
+++ database/seeders/DogsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DogsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('dogs')->insert(['name' => 'Joe']);
+        DB::table('dogs')->insert(['name' => 'Jock']);
+        DB::table('dogs')->insert(['name' => 'Jackie']);
+        DB::table('dogs')->insert(['name' => 'Jane']);
+    }
+}
